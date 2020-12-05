ce5a2a3f71a4db9c741add3cea95a43bd475ddc0
Scott Adkin <scottadkin001@gmail.com>
2020-12-05 12:13:04 +0000

    Version 1.0

---
+++ api/umodextractor.js
@@ -0,0 +1,155 @@
+const fs = require('fs');
+
+
+
+class UModExtractor{
+
+    constructor(file){
+
+        console.log(`[Note]: Starting unpack of package ${file}.`);
+        this.file = file;
+
+        try{
+            this.buffer = fs.readFileSync(this.file);
+        }catch(err){
+            console.log(`[ERROR]: Failed to open file ${file}: ${err.code}`);
+            console.log(`[ERROR]: Files must be entered in with comma separated list without the file extension as follows: fileone, filetwo`);
+            return;
+        }
+        this.logoLine = null;
+        this.startIndex = 0;
+
+        this.setupLines = [];
+
+        this.files = [];
+   
+        this.createSetupLines();
+
+        if(this.logoLine !== null){
+
+            // + 2 for newline
+            this.startIndex = this.buffer.indexOf(this.logoLine) + this.logoLine.length + 2; 
+
+        }
+
+        this.createSetup();
+
+        this.createFiles();
+        
+    }
+
+    createSetup(){   
+
+        //File=(Src=System\TIMMYVB.u,Size=1839442)
+        const fileReg = /^file=\(src=(.+),size=(\d+)\)$/i;
+
+        let result = 0;
+
+        let s = 0;
+        let offset = 0;
+
+        for(let i = 0; i < this.setupLines.length; i++){
+
+            s = this.setupLines[i];
+
+            if(fileReg.test(s)){
+
+                result = fileReg.exec(s);
+
+                this.files.push(
+                    {
+                        "src": result[1],
+                        "size": parseInt(result[2]),
+                        "offset": offset
+                    }
+                );
+
+                offset += parseInt(result[2])
+            }
+        }
+
+       // console.log(this.files);
+    }
+
+    createSetupLines(){
+
+        const data = [];
+
+        const test = this.buffer.toString();
+
+        const reg = /^(.+?)$/img;
+
+        const lines = test.match(reg)
+        //console.log(lines);
+
+        const blockReg = /^\[(.+?)\]$/i;
+        const keyValueReg = /(.+?)=.+?/i;
+
+        //the second [setup] is the last block we need
+        let currentSetupBlock = 0;
+
+        let result = 0;
+
+        for(let i = 0; ; i++){
+
+            if(blockReg.test(lines[i])){
+
+                if(currentSetupBlock >= 2){
+                    break;
+                }
+
+                this.setupLines.push(lines[i]);
+
+                result = blockReg.exec(lines[i]);
+
+                if(result[1].toLowerCase() === 'setup'){
+                    currentSetupBlock++;
+                }
+
+            }else if(keyValueReg.test(lines[i])){
+                this.setupLines.push(lines[i]);
+
+                result = keyValueReg.exec(lines[i]);
+
+                if(result[1] === 'Logo'){
+                    this.logoLine = lines[i];
+                }
+            }
+        }
+    }
+
+    createFiles(){
+
+
+        let f = 0;
+
+        let currentData = 0;
+
+        const dirReg = /^(.+)(\/|\\).+\..+$/;
+        let dirResult = 0;
+
+        try{
+
+            for(let i = 0; i < this.files.length; i++){
+
+                f = this.files[i];
+
+                dirResult = dirReg.exec(f.src);
+
+                fs.mkdirSync(dirResult[1], {recursive: true});
+                currentData = this.buffer.slice(f.offset + this.startIndex, f.offset + f.size + this.startIndex);
+                console.log(`[Note]: Creating file ${dirResult[1]}/${f.src}`);
+                fs.writeFileSync(f.src, currentData);
+
+            }
+
+            console.log(`[Pass]: Finished unpacking of ${this.file}.`);
+
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+}
+
+module.exports = UModExtractor;
--- app.js
+++ app.js
@@ -1,134 +1,83 @@
 const fs = require('fs');
+const UModExtractor = require('./api/umodextractor');
 
 
-class UModExtractor{
 
-    constructor(file){
+function displayStartUpMessages(){
 
-        this.file = file;
+    console.log(`[--------------------------------------------------------]`);
 
-        this.buffer = fs.readFileSync(file);
-        this.logoLine = null;
-        this.startIndex = 0;
+    console.log(`[--------------Unreal Tournament UMOD extractor----------]`);
+    console.log(`[--------------------Version 1.0-------------------------]`);
+    console.log(`[-----------------5th December 2020----------------------]`);
+    console.log(`[---------------Created By Scott Adkin-------------------]`);
+    console.log(`[----------------github.com/scottadkin-------------------]`);
+    
+    console.log(`[--------------------------------------------------------]`);
 
-        this.setupLines = [];
-
-        this.files = [];
-   
-        this.createSetupLines();
-
-        if(this.logoLine !== null){
-
-            // + 2 for newline
-            this.startIndex = this.buffer.indexOf(this.logoLine) + this.logoLine.length + 2; 
-
-        }
-
-        this.createSetup();
-
-        this.createFiles();
-        
-    }
-
-    createSetup(){
-
-        
-
-        //File=(Src=System\TIMMYVB.u,Size=1839442)
-        const fileReg = /^file=\(src=(.+),size=(\d+)\)$/i;
-
-        let result = 0;
-
-        let s = 0;
-        let offset = 0;
-
-        for(let i = 0; i < this.setupLines.length; i++){
-
-            s = this.setupLines[i];
-
-            if(fileReg.test(s)){
-
-                result = fileReg.exec(s);
-
-                this.files.push(
-                    {
-                        "src": result[1],
-                        "size": parseInt(result[2]),
-                        "offset": offset
-                    }
-                );
-
-                offset += parseInt(result[2])
-            }
-        }
-
-       // console.log(this.files);
-    }
-
-    createSetupLines(){
-
-        const data = [];
-
-        const test = this.buffer.toString();
-
-        const reg = /^(.+?)$/img;
-
-        const lines = test.match(reg)
-        //console.log(lines);
-
-        const blockReg = /^\[(.+?)\]$/i;
-        const keyValueReg = /(.+?)=.+?/i;
-
-        //the second [setup] is the last block we need
-        let currentSetupBlock = 0;
-
-        let result = 0;
-
-        for(let i = 0; ; i++){
-
-            if(blockReg.test(lines[i])){
-
-                if(currentSetupBlock >= 2){
-                    break;
-                }
-
-                this.setupLines.push(lines[i]);
-
-                result = blockReg.exec(lines[i]);
-
-                if(result[1].toLowerCase() === 'setup'){
-                    currentSetupBlock++;
-                }
-
-            }else if(keyValueReg.test(lines[i])){
-                this.setupLines.push(lines[i]);
-
-                result = keyValueReg.exec(lines[i]);
-
-                if(result[1] === 'Logo'){
-                    this.logoLine = lines[i];
-                }
-            }
-        }
-    }
-
-    createFiles(){
-
-
-        let f = 0;
-
-        let currentData = 0;
-
-        for(let i = 0; i < this.files.length; i++){
-
-            f = this.files[i];
-
-            currentData = this.buffer.slice(f.offset + this.startIndex, f.offset + f.size + this.startIndex);
-            fs.writeFileSync(f.src, currentData);
-
-        }
-    }
 }
 
 
-const test = new UModExtractor('Butters Version 2.umod');
+
+
+
+function run(){
+
+    try{
+
+        const args = process.argv;
+
+        if(args.length >= 3){
+
+            const filesToProcess = [];
+
+            const commaReg = /,/i;
+
+            let currentFile = '';
+
+            for(let i = 2; i < args.length; i++){
+
+                if(commaReg.test(args[i])){
+
+                    currentFile += ` ${args[i]}`;
+                    filesToProcess.push(currentFile);
+                    currentFile = '';
+
+                }else{
+                    if(currentFile !== ''){
+                        currentFile += ` ${args[i]}`;
+                    }else{
+                        currentFile = args[i];
+                    }
+                }
+
+            }
+
+            if(currentFile !== ''){
+                filesToProcess.push(currentFile);
+            }
+
+            //remove commas from names
+            for(let i = 0; i < filesToProcess.length; i++){
+
+                filesToProcess[i] = filesToProcess[i].replace(',','').trim();
+            }
+
+            for(let i = 0; i < filesToProcess.length; i++){
+
+                new UModExtractor(`${filesToProcess[i]}.umod`);
+            }
+
+        }else{
+            throw new error('[ERROR]: No file was specified.');
+        }
+
+    }catch(err){
+        console.log(err);
+    }
+
+}
+
+
+displayStartUpMessages();
+run();
